1ae151a1153333a5f8e470f12a5b6f536e87a0e9
no <60884353+James-261@users.noreply.github.com>
2021-03-04 11:20:04 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,16 @@ Logger& getLogger()
     return *logger;
 }
 
-MAKE_HOOK_OFFSETLESS(Player_Start, void, Il2CppObject* self)
+MAKE_HOOK_OFFSETLESS(Player_Awake, void, Il2CppObject* self)
 {
-    INFO("Player Start Hook Called!");
+    INFO("Player Awake Hook Called!");
 
     Il2CppString* csName = CRASH_UNLESS(il2cpp_utils::GetFieldValue<Il2CppString*>(self, "savedName"));
     std::string name = to_utf8(csstrtostr(csName));
 
     getLogger().info("Name was: %s", name);
     CRASH_UNLESS(il2cpp_utils::SetFieldValue<Il2CppString*>(self, "savedName", il2cpp_utils::createcsstr("newName"))); 
-    Player_Start(self);
+    Player_Awake(self);
 }
 
 extern "C" void setup(ModInfo& info)
@@ -45,7 +45,7 @@ extern "C" void load()
     il2cpp_functions::Init();
     INFO("Installing hooks...");
 
-    INSTALL_HOOK_OFFSETLESS(logger, Player_Start, il2cpp_utils::FindMethodUnsafe("GorillaComputer", "Player", "Awake", 0));
+    INSTALL_HOOK_OFFSETLESS(logger, Player_Awake, il2cpp_utils::FindMethodUnsafe("GorillaComputer", "Player", "Awake", 0));
 
     INFO("Installed hooks!");
 }
